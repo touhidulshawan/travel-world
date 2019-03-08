001d797b9b0a14ff5d880c32bad90a6046bcb14c
touhidulShawan <touhidulislamshaon@gmail.com>
2019-03-08 17:26:57 +0600

    adding all stuff

---
+++ src/components/headers/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../assets/css/Header.min.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-box">TW</div>
+      <div className="header-content">
+        <h1 className="primary-text">
+          <span className="primary-text-main"> Adventurous </span>
+          <span className="primary-text-sub">
+            Dare to live the life you’ve always wanted
+          </span>
+        </h1>
+        <a href="/" className="btn btn-red btn-animated">
+          Let Explore <i class="fas fa-arrow-down    " />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "../components/headers/Header";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
