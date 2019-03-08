c8df10457e76e9921419eca6c075b8fdd402f9c7
touhidulShawan <touhidulislamshaon@gmail.com>
2019-03-08 18:29:08 +0600

    changes class name followed by BEM

--- src/components/headers/Header.js
+++ src/components/headers/Header.js
@@ -4,15 +4,15 @@ import "../../assets/css/Header.min.css";
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-box">TW</div>
-      <div className="header-content">
+      <div className="header__logo-box">TW</div>
+      <div className="header__content">
         <h1 className="primary-text">
-          <span className="primary-text-main"> Adventurous </span>
-          <span className="primary-text-sub">
+          <span className="primary-text--main"> Adventurous </span>
+          <span className="primary-text--sub">
             Dare to live the life you’ve always wanted
           </span>
         </h1>
-        <a href="/" className="btn btn-red btn-animated">
+        <a href="/" className="btn btn--red btn--animated">
           Let Explore <i class="fas fa-arrow-down    " />
         </a>
       </div>
