a6f7dad17de5adfe1e6f1d08948f5a2acd13e922
touhidulShawan <touhidulislamshaon@gmail.com>
2019-03-15 13:55:17 +0600

    complete footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,9 +3,48 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <h1>Footer</h1>
-      </div>
+      <section className="footer-section">
+        <div className="row">
+          <div className="col-1-of-2">
+            <ul>
+              <li>
+                <a href="/" target="_blank">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="/" target="_blank">
+                  Project
+                </a>
+              </li>
+              <li>
+                <a href="/" target="_blank">
+                  Recent Tour
+                </a>
+              </li>
+              <li>
+                <a href="/" target="_blank">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-1-of-2">
+            <h4>Lorem ipsum dolor sit amet</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae minus sunt quasi, itaque impedit ab molestiae
+              accusamus nesciunt.
+            </p>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae minus sunt quasi, itaque impedit ab molestiae
+              accusamus nesciunt.
+            </p>
+          </div>
+        </div>
+      </section>
     );
   }
 }
