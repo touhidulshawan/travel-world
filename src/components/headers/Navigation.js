3afb4d9b5423202d0ffdeb6e48b4ae1b9cf76506
touhidulShawan <touhidulislamshaon@gmail.com>
2019-03-16 12:53:59 +0600

    complete navigation

--- src/components/headers/Navigation.js
+++ src/components/headers/Navigation.js
@@ -2,8 +2,46 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <div>
-      <h1>Nav</h1>
+    <div className="navigation">
+      <input
+        type="checkbox"
+        id="navigation__toggle"
+        className="navigation__checkbox"
+      />
+      <label htmlFor="navigation__toggle" className="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
+
+      <div className="navigation__background">&nbsp;</div>
+      <nav className="navigation__nav">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <a href="/" className="navigation__link">
+              Home
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="/" className="navigation__link">
+              About
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="/" className="navigation__link">
+              Recent Tour
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="/" className="navigation__link">
+              Stories
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="/" className="navigation__link">
+              Book Now
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
