f93005314f5184f5446d77e1c946bf286150ada4
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-03-21 00:39:07 +0600

    Updates

---
+++ precache-manifest.5ca76c3e7b7131be6933c28db4adc4fa.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/travel-world/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "6b2ada4f41ced4da4f1b",
+    "url": "/travel-world/static/css/main.c2ace2e0.chunk.css"
+  },
+  {
+    "revision": "c41a9c7ba69d401d0b8a",
+    "url": "/travel-world/static/js/runtime~main.0c6f7922.js"
+  },
+  {
+    "revision": "c5fbe595c8f61f8067c1",
+    "url": "/travel-world/static/js/2.4e30a3a4.chunk.js"
+  },
+  {
+    "revision": "91995749c53780a23d0dc8d5ecea546d",
+    "url": "/travel-world/static/media/Travel-proj_2.91995749.jpeg"
+  },
+  {
+    "revision": "14c93229319385ae21e84e58cad66159",
+    "url": "/travel-world/static/media/Travel-proj_7.14c93229.jpeg"
+  },
+  {
+    "revision": "b022a7fad2e855f01917fca08ca9929b",
+    "url": "/travel-world/static/media/Travel-proj_8.b022a7fa.jpeg"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/travel-world/static/media/video.ffcd876c.mp4"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/travel-world/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "6b2ada4f41ced4da4f1b",
+    "url": "/travel-world/static/js/main.c9acf00a.chunk.js"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/travel-world/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/travel-world/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/travel-world/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "16648b76a4e1dc886063ece074432d7e",
+    "url": "/travel-world/static/media/Travel-proj_9.16648b76.jpeg"
+  },
+  {
+    "revision": "bbb9b62105bce3b034a538ecd62fb8f1",
+    "url": "/travel-world/static/media/Travel-proj_5.bbb9b621.jpeg"
+  },
+  {
+    "revision": "1929421d706f7166bde414772ac173c2",
+    "url": "/travel-world/static/media/Banner.1929421d.jpeg"
+  },
+  {
+    "revision": "242823235cd215bd67639833b02fde10",
+    "url": "/travel-world/static/media/Travel-proj_4.24282323.jpeg"
+  },
+  {
+    "revision": "0007323e90fdc1e0d3c37ac07be7ba7e",
+    "url": "/travel-world/static/media/Travel-proj_3.0007323e.jpeg"
+  },
+  {
+    "revision": "c8c58a691a69fc0c8894170ddb80b6e6",
+    "url": "/travel-world/index.html"
+  }
+];
